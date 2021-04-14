0be571876fc4cdcc8c582c5e603523cc62762930
Max Patiiuk <max@patii.uk>
2021-04-14 21:54:37 +0300

    Rewrite the `regex-blacklist` hook to use git grep

--- pre-commit/regex-blacklist.sh
+++ pre-commit/regex-blacklist.sh
@@ -6,15 +6,33 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
-dictionary="${1}"
+dictionary="specifyweb/frontend/js_src/lib/tests/regex-blacklist.txt"
+#dictionary="${1}"
 changed_files="${@:2}"
 
+if [ ! -f $dictionary ]; then
+  echo -e "${RED}Dictionary file not found!${NC}"
+  exit 1
+fi
+
+filtered_files=()
 for file in $changed_files; do
-  if ["$dictionary" != "$file"] && (grep "$file" -E -C 4 -n --color -f "$dictionary");
-  then
-    echo -e "${RED}Found blacklisted matches in \"$file\"${NC}"
-    exit 1
+  if [[ $file != $dictionary ]]; then
+    filtered_files+=($file)
   fi
 done
+
+if [ ${#filtered_files[@]} -eq 0 ]; then
+  echo "No files to check"
+  exit 0
+fi
+
+found=`git grep --cached -E -C 4 -n --color --break -f "$dictionary" -- $filtered_files`
+
+if [[ ! -z $found ]]; then
+  echo -e "$found"
+  echo -e "${RED}Some files matched blacklisted strings${NC}"
+  exit 1
+fi
 
 exit 0
